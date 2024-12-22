cac2088b99bfe8b82e76d4c030d913c8700eff04
heyvj <vijayalaxmi9915@gmail.com>
2024-12-21 23:00:43 -0800

    fix_failures

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
 import App from "./App.jsx";
-import { TempApp } from "./TempApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
